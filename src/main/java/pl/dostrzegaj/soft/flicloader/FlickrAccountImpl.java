3aa489c69d220314935d7440e87e6941b59daf9d
Jaroslaw Jarmolowicz <jaroslaw.jarmolowicz@tomtom.com>
2016-10-02 08:19:35 +0200

    LEts do something about server communication

--- src/main/java/pl/dostrzegaj/soft/flicloader/FlickrAccountImpl.java
+++ src/main/java/pl/dostrzegaj/soft/flicloader/FlickrAccountImpl.java
@@ -1,12 +1,15 @@
 package pl.dostrzegaj.soft.flicloader;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import org.scribe.exceptions.OAuthConnectionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.flickr4java.flickr.Flickr;
 import com.flickr4java.flickr.FlickrException;
+import com.flickr4java.flickr.FlickrRuntimeException;
 import com.flickr4java.flickr.RequestContext;
 import com.flickr4java.flickr.auth.Auth;
 import com.flickr4java.flickr.photosets.PhotosetsInterface;
@@ -20,6 +23,8 @@ class FlickrAccountImpl implements UserAccount {
     private static final Logger LOGGER = LoggerFactory.getLogger(FlickrAccountImpl.class);
     private Flickr f;
     private Auth auth;
+    private int SEND_RETRIES = 3;
+    private long SEND_RETRy_DELAY_BASE = TimeUnit.SECONDS.toMillis(1);
 
     public FlickrAccountImpl(Flickr f, Auth auth) {
         this.f = f;
@@ -56,8 +61,28 @@ class FlickrAccountImpl implements UserAccount {
             metaData.setFilename(basefilename);
 
             try {
-                String photoId = uploader.upload(photo.getFile(), metaData);
-                result.add(new UploadedPhoto(photoId, photo.getRelativePath()));
+                for (int i = 1; i <= SEND_RETRIES; ++i) {
+                    RuntimeException ex = null;
+                    try {
+                        String photoId = uploader.upload(photo.getFile(), metaData);
+                        result.add(new UploadedPhoto(photoId, photo.getRelativePath()));
+                        break;
+                    } catch (OAuthConnectionException oce) {
+                        ex = oce;
+                    } catch (FlickrRuntimeException fre) {
+                        ex = fre;
+                    }
+                    if (i < SEND_RETRIES) {
+                        LOGGER.warn("communication error, retriable", ex);
+                        try {
+                            Thread.sleep(i * SEND_RETRy_DELAY_BASE);
+                        } catch (InterruptedException ie) {
+                            Throwables.propagate(ie);
+                        }
+                    } else {
+                        throw ex;
+                    }
+                }
             } catch (FlickrException e) {
                 LOGGER.error("Error during flickr uplaoding", e);
             }
